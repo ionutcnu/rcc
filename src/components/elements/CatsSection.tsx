c7b38ac7bb4dee0b2fafc93437dc971fbc94d0f0
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2024-09-06 02:16:50 +0300

    fixed wrong edititing cats section v2

--- src/components/elements/CatsSection.tsx
+++ src/components/elements/CatsSection.tsx
@@ -10,12 +10,6 @@ import SwiperCore from 'swiper';
 export default function CatsSection() {
     const swiperRef = useRef<SwiperCore>();
 
-    // const cats = [
-    //     { id: 1, name: "Gura Mare", alias: "Gura", description: "Very active and playful, Misty is your trusting buddy when it comes to keeping rats away!", image: "/pages/cats/cat1.jpg" },
-    //     { id: 2, name: "Veve", alias: "Veve", description: "She's grumpy and always demands food, but also likes to cuddle in the couch or bed.", image: "/pages/cats/cat3.jpg" },
-    //     { id: 3, name: "Ollie", alias: "Ollie", description: "He likes to run around and play hide-and-seek. He also loves head and belly rubs!", image: "/pages/cats/cat1.jpg" },
-    //     { id: 4, name: "Misty", alias: "Misty", description: "Very active and playful, Misty is your trusting buddy when it comes to keeping rats away!", image: "/pages/cats/cat2.jpg" },
-    // ];
 
     return (
         <section className="relative bg-[#1C1C21] text-white py-16">
@@ -43,7 +37,7 @@ export default function CatsSection() {
                             <Link href={`/pages/cats/${cat.alias}`} key={cat.id}>
                                 <div className="bg-white text-black rounded-lg overflow-hidden shadow-lg cursor-pointer">
                                     <img
-                                        src={cat.image}
+                                        src={cat.mainImage}
                                         alt={cat.name}
                                         className="object-cover h-56 w-full"
                                     />
