5dd6d35e819ee9b57877f6bf95f21d456559ae97
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2024-09-06 22:59:36 +0300

    added allcat

--- src/app/data/catsData.ts
+++ src/app/data/catsData.ts
@@ -6,17 +6,17 @@ export const cats = [
         description: "Mare matza alba",
         mainImage: "/Cats/Images/cat1.jpg",
         images: [
-    "/Cats/Images/cat1.jpg",
-    "/Cats/Images/cat3.jpg",
-    "/Cats/Images/indian.png",
-    "/Cats/Images/catm.jpg",
-    "/Cats/Images/cat3.jpg",
-    "/Cats/Images/cat1.jpg",
-    "/Cats/Images/cat1.jpg"
-],
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg",
+            "/Cats/Images/cat1.jpg"
+        ],
         videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
         color: "White",
         gender: "Female",
+        yearOfBirth: 2017
     },
     {
         id: 2,
@@ -24,9 +24,11 @@ export const cats = [
         name: "Berberita",
         description: "Se urca in copac dupa ghinde",
         mainImage: "/Cats/Images/cat1.jpg",
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
         images: ["/Cats/cat1.jpg", "/Cats/catm.jpg", "/Cats/cat3.jpg"],
         gender: "Female",
         color: "Orange",
+        yearOfBirth: 2016
     },
     {
         id: 3,
@@ -34,9 +36,11 @@ export const cats = [
         name: "Mancau",
         description: "Mananca si doarme, atata stie",
         mainImage: "/Cats/Images/cat1.jpg",
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
         images: ["/Cats/cat1.jpg", "/Cats/catm.jpg", "/Cats/cat3.jpg"],
         gender: "Male",
         color: "Orange and black",
+        yearOfBirth: 2015
     },
     {
         id: 4,
@@ -44,8 +48,106 @@ export const cats = [
         name: "LONUT",
         description: "Born to be wild, forced to hello LONUT",
         mainImage: "/Cats/Images/cat3.jpg",
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
         images: ["/Cats/cat1.jpg", "/Cats/catm.jpg", "/Cats/cat3.jpg"],
         gender: "Male",
         color: "Brown",
+        yearOfBirth: 2014
     },
+    {
+        id: 5,
+        alias: "snowball",
+        name: "Snowball",
+        description: "Fluffy and white, enjoys chasing snowflakes",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "White",
+        gender: "Female",
+        yearOfBirth: 2018
+    },
+    {
+        id: 6,
+        alias: "tiger",
+        name: "Tiger",
+        description: "Loves to pounce and play, fearless hunter",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "Gray",
+        gender: "Male",
+        yearOfBirth: 2016
+    },
+    {
+        id: 7,
+        alias: "mocha",
+        name: "Mocha",
+        description: "Energetic and loves to run around the house",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "Brown",
+        gender: "Male",
+        yearOfBirth: 2017
+    },
+    {
+        id: 8,
+        alias: "princess",
+        name: "Princess",
+        description: "Regal and majestic, enjoys being pampered",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "Golden",
+        gender: "Female",
+        yearOfBirth: 2015
+    },
+    {
+        id: 9,
+        alias: "shadow",
+        name: "Shadow",
+        description: "Quiet and mysterious, prefers the dark corners",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "Black",
+        gender: "Male",
+        yearOfBirth: 2014
+    },
+    {
+        id: 10,
+        alias: "mittens",
+        name: "Mittens",
+        description: "Loves to cuddle, especially during cold nights",
+        mainImage: "/Cats/Images/cat1.jpg",
+        images: [
+            "/Cats/Images/cat1.jpg",
+            "/Cats/Images/cat3.jpg",
+            "/Cats/Images/catm.jpg"
+        ],
+        videos: ["/Cats/Videos/Video1.mp4", "/Cats/Videos/Video2.mp4"],
+        color: "White with black spots",
+        gender: "Female",
+        yearOfBirth: 2018
+    }
 ];
