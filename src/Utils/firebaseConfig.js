46d253eb68fb7820d6e45708395d60f8206abdd6
Aeon <25122138+ionutcnu@users.noreply.github.com>
2024-11-01 10:18:03 +0200

    fix firebase deploy v5

--- src/Utils/firebaseConfig.js
+++ src/Utils/firebaseConfig.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    apiKey: process.env.API_KEY,
     authDomain: "redcatcuasar.firebaseapp.com",
     projectId: "redcatcuasar",
     storageBucket: "redcatcuasar.appspot.com",
