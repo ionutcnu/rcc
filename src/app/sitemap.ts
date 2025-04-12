b2885e045b343270138d791afd76e791d3de71d8
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2025-04-12 22:23:28 +0300

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,29 +3,24 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     const routes = [
         {
-            url: 'https://quests.so/',
+            url: 'rcc-kappa.vercel.app',
             lastModified: new Date().toISOString(),
             priority: 1.0,
             changeFrequency: 'weekly' as 'weekly'
         },
         {
-            url: 'https://quests.so/contact',
+            url: 'rcc-kappa.vercel.app/contact',
             lastModified: new Date().toISOString(),
             priority: 0.8,
             changeFrequency: 'monthly' as 'monthly'
         },
         {
-            url: 'https://quests.so/privacy',
+            url: 'rcc-kappa.vercel.app/cats',
             lastModified: new Date().toISOString(),
             priority: 0.5,
             changeFrequency: 'yearly' as 'yearly'
         },
-        {
-            url: 'https://quests.so/terms',
-            lastModified: new Date().toISOString(),
-            priority: 0.5,
-            changeFrequency: 'yearly' as 'yearly'
-        }
+
     ];
 
     return routes;
