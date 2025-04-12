08c624658a4b1a7125a3a80ca04fac4409ee11df
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2025-04-13 00:21:02 +0300

    fixed seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,17 @@ const patrickHand = Patrick_Hand({
     display: "swap",
 })
 
-
-export const metadata = {
+export const metadata: Metadata = {
     title: "Meet Our Gorgeous Red Cats | Family Tree & Videos Included",
-    description: "Adopt beautiful red cats with full lineage info, stunning photos, and short videos. Find your next feline companion today.",
+    description:
+        "Adopt beautiful red cats with full lineage info, stunning photos, and short videos. Find your next feline companion today.",
+    metadataBase: new URL("https://rcc-kappa.vercel.app"),
     openGraph: {
-        title: "Meet Our Gorgeous Red Cats | Family Tree & Videos Included",
-        description: "Adopt beautiful red cats with full lineage info, stunning photos, and short videos. Find your next feline companion today.",
-        url: "https://rcc-kappa.vercel.app/",
+        url: "https://rcc-kappa.vercel.app",
         type: "website",
+        title: "Meet Our Gorgeous Red Cats | Family Tree & Videos Included",
+        description:
+            "Adopt beautiful red cats with full lineage info, stunning photos, and short videos. Find your next feline companion today.",
         images: [
             {
                 url: "https://firebasestorage.googleapis.com/v0/b/redcatcuasar.firebasestorage.app/o/cats%2Fimages%2F6252282e-ca88-4a46-a22b-b608aa53859e-image-1744398927041.jpeg?alt=media&token=6d9667c3-36f8-4dc8-bb57-33407e457de3",
@@ -38,7 +40,15 @@ export const metadata = {
             },
         ],
     },
-};
+
+    alternates: {
+        canonical: "https://rcc-kappa.vercel.app",
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+}
 
 export default function RootLayout({
                                        children,
@@ -54,7 +64,7 @@ export default function RootLayout({
         <body className="font-patrick">
         {children}
         <Analytics />
-        <SpeedInsights/>
+        <SpeedInsights />
         </body>
         </html>
     )
