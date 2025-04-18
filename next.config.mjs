619f4cbc40829393e934b66fffb089a42335769b
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2025-04-18 21:19:51 +0300

    login fix on production v7

--- next.config.mjs
+++ next.config.mjs
@@ -12,20 +12,19 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // Apply these headers to all routes
-                source: '/:path*',
+                source: '/(.*)',
                 headers: [
                     {
-                        key: 'Cross-Origin-Opener-Policy',
-                        value: 'same-origin',
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'credentialless',
                     },
                     {
-                        key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
+                        key: 'Cross-Origin-Resource-Policy',
+                        value: 'cross-origin',
                     },
                 ],
             },
-        ]
+        ];
     },
 };
 
