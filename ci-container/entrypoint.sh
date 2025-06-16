9eef09dfc775ab4267099d1e0a4fa3a203a2e992
ionutcnu <25122138+ionutcnu@users.noreply.github.com>
2025-06-17 00:04:34 +0300

    test

--- ci-container/entrypoint.sh
+++ ci-container/entrypoint.sh
@@ -28,7 +28,9 @@ bun run start &
 SERVER_PID=$!
 wait-on http://localhost:3000
 
-COMMIT_MESSAGE="$INPUT_COMMIT_MESSAGE"
+export COMMIT_INFO_MESSAGE="${COMMIT_MESSAGE}"
+export COMMIT_INFO_SHA="${GITHUB_SHA:-}"
+export COMMIT_INFO_BRANCH="${GITHUB_REF_NAME:-}"
 
 # 5) Run Cypress with JUnit reporter into the workspace
 mkdir -p /github/workspace/results
